7bf420808ccb994d8c9928efa1e72854cd3a8210
Carl Parrish <cparrish@deeplydigital.net>
2024-05-07 08:12:06 -0700

    refactor: life state up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,12 +2,29 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 
+import { useState } from "react";
+
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuthorHandler}
+      />
+
       <ul className={styles.posts}>
-        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
         <Post author="Julie" body="I love React.js!" />
       </ul>
