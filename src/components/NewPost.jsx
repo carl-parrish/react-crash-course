7090895c576edc105ef4cdd9a9a52fdf80d9e2cf
Carl Parrish <cparrish@deeplydigital.net>
2024-05-07 07:26:55 -0700

    poc: using event change to update state

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,20 @@
-import classes from "./NewPost.module.css";
+import { useState } from "react";
+import styles from "./NewPost.module.css";
 
 function NewPost() {
+  const [enteredBody, setEnteredBody] = useState("");
+
   function changeBodyHandler(event) {
-    console.log(event.target.value);
+    setEnteredBody(event.target.value);
   }
 
   return (
-    <form className={classes.form}>
+    <form className={styles.form}>
       <p>
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
-import Post from './Post';
-import styles from './PostsList.module.css';
+import NewPost from "./NewPost";
+import Post from "./Post";
+import styles from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={styles.posts}>
+    <>
+      <NewPost />
+      <ul className={styles.posts}>
         <Post author="Maximilian" body="React.js is awesome!" />
         <Post author="Manuel" body="Check out the full course!" />
         <Post author="Julie" body="I love React.js!" />
-    </ul>
+      </ul>
+    </>
   );
 }
 
